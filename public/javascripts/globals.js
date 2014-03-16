958dd6a25b59280a64f640d647daefece6e9945e
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-03-16 19:53:56 -0400

    Fix: optional param handling

--- public/javascripts/globals.js
+++ public/javascripts/globals.js
@@ -4,10 +4,10 @@ document.createElement('section');
 // Centres element in window.
 jQuery.fn.centre = function (horizontal, vertical) {
   this.css("position","absolute");
-  if (horizontal)
+  if (horizontal || horizontal === undefined)
     this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2)
           + $(window).scrollLeft()) + "px");
-  if (vertical)
+  if (vertical || vertical === undefined)
     this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2)
                              + $(window).scrollTop()) + "px");
   return this;
