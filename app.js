97097be2433c25219bd4f11efe9ed851b52d44b2
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-05-20 21:53:13 -0400

    Refactoring around routes

--- app.js
+++ app.js
@@ -4,19 +4,16 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
-var http = require('http');
 var path = require('path');
-var fs = require('fs');
-var routes = require('./lib/routes.js');
+var routes = require('./lib/router');
 
 var defaultPort = 3000;
-var homePage = fs.readFileSync(path.join(__dirname, 'public/index.html'),
-                               {encoding: 'utf8'});
+var port = process.env.PORT || defaultPort;
 
 console.log('[START] '.yellow + 'Hockey Oracle start-up');
 
 var app = initExpress();
-http.createServer(app).listen(app.get('port'), onServerStarted);
+app.listen(port, onServerStarted);
 
 // Configure and initialise Express.
 function initExpress()
@@ -25,34 +22,18 @@ function initExpress()
 
   var app = express();
 
-  app.set('port', process.env.PORT || defaultPort);
   app.set('view options', {layout: false});
-  app.set('isDev', app.get('env') == 'development');
+  if (app.get('env') == 'development')
+  {
+    console.log('Running in *DEVELOPMENT* mode.'.blue);
+    app.set('isDev', true);
+  }
 
   app.use(morgan(app.get('isDev') ? 'dev' : ''));
   app.use(bodyParser());
   app.use(methodOverride());
   app.use(express.static(path.join(__dirname, 'public')));
-
-  if (app.get('isDev'))
-  {
-    console.log('Running in *DEVELOPMENT* mode.'.blue);
-  }
-
-  // View Routes
-  app.get('/', sendHome);
-  app.get('/players', sendHome);
-  app.get('/players/randomize', sendHome);
-  app.get('/teams', sendHome);
-  function sendHome(req, res)
-  {
-    res.send(homePage);
-  }
-
-  // API Routes
-  app.get('/api/players', routes.players);
-  app.get('/api/players/randomize', routes.randomize);
-  app.post('/api/players/active', routes.updateActivePlayers);
+  app.use('/', routes);
 
   // Error handling
   app.use(errorHandler());
@@ -63,6 +44,6 @@ function initExpress()
 
 function onServerStarted()
 {
-  console.log('Web server started and listening on port ' + app.get('port') + '.');
+  console.log('Web server started and listening on port ' + port + '.');
   console.log('[END] '.green + 'Hockey Oracle start-up');
 }
