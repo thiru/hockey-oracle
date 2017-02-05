2b39348732e0e3a10f42b4f4692b14455f9a54cb
Thiru Thirunavukarasu <thiru0130@gmail.com>
2017-02-04 19:56:50 -0500

    Optimise code according to environment

--- app.lisp
+++ app.lisp
@@ -1,5 +1,17 @@
 (in-package :hockey-oracle)
 
+;;; Optimise code for a production environment unless the respective development environment variable is set
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defparameter *optimize-default*
+    (if (uiop/os:getenvp "LISP_DEV")
+      '(optimize (debug 3) (safety 3) (speed 0))
+      '(optimize (debug 0) (safety 1) (speed 3)))))
+
+(format t "~%Optimize levels: ~A~%" *optimize-default*)
+
+(declaim #.*optimize-default*)
+
 ;;; Utils
 (defun first1 (obj)
   "Gets the first item in OBJ if it's a list, otherwise OBJ is simply returned."
